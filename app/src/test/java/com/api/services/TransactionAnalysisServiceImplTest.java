f715d54bbc73a3f0cacfd3e70df8ea9f5871f1ec
Rudy Patel <rutvik@ualberta.ca>
2023-06-24 15:06:30 -0600

    added TransactionAnalysisServiceImplTest

---
+++ app/src/test/java/com/api/services/TransactionAnalysisServiceImplTest.java
@@ -0,0 +1,117 @@
+package com.api.services;
+
+import com.api.models.Transaction;
+import com.api.models.TransactionAnalysisResponse;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TransactionAnalysisServiceImplTest {
+
+    private TransactionAnalysisService transactionAnalysisService;
+
+    @BeforeEach
+    public void setup() {
+        transactionAnalysisService = new TransactionAnalysisServiceImpl();
+    }
+
+    @Test
+    public void testAnalyzeTransaction_ValidTransaction_ReturnsApprovedResponse() {
+        // Arrange
+        Transaction transaction = new Transaction();
+        transaction.setCardNum(1234567890123456L);
+        transaction.setAmount(100.0);
+
+        List<Integer> cardUsageCounts = Arrays.asList(5, 5, 5, 5, 5, 5, 5);
+
+        // Act
+        TransactionAnalysisResponse response = transactionAnalysisService.analyzeTransaction(transaction, cardUsageCounts);
+
+        // Assert
+        assertEquals("Approved", response.getTransactionStatus());
+        assertEquals("1234********3456", response.getCardNumber());
+        assertEquals(100.0, response.getTransactionAmount());
+        assertEquals(35, response.getCardUsageCount());
+    }
+
+    @Test
+    public void testAnalyzeTransaction_AmountOverThreshold_ReturnsDeclinedResponse() {
+        // Arrange
+        Transaction transaction = new Transaction();
+        transaction.setCardNum(1234567890123456L);
+        transaction.setAmount(60000.0);
+
+        List<Integer> cardUsageCounts = Arrays.asList(10, 15, 20, 10, 5, 15, 20);
+
+        // Act
+        TransactionAnalysisResponse response = transactionAnalysisService.analyzeTransaction(transaction, cardUsageCounts);
+
+        // Assert
+        assertEquals("Declined", response.getTransactionStatus());
+        assertEquals("1234********3456", response.getCardNumber());
+        assertEquals(60000.0, response.getTransactionAmount());
+        assertEquals(95, response.getCardUsageCount());
+    }
+
+    @Test
+    public void testAnalyzeTransaction_CardUsageOverThreshold_ReturnsDeclinedResponse() {
+        // Arrange
+        Transaction transaction = new Transaction();
+        transaction.setCardNum(1234567890123456L);
+        transaction.setAmount(100.0);
+
+        List<Integer> cardUsageCounts = Arrays.asList(70, 15, 20, 10, 5, 15, 20);
+
+        // Act
+        TransactionAnalysisResponse response = transactionAnalysisService.analyzeTransaction(transaction, cardUsageCounts);
+
+        // Assert
+        assertEquals("Declined", response.getTransactionStatus());
+        assertEquals("1234********3456", response.getCardNumber());
+        assertEquals(100.0, response.getTransactionAmount());
+        assertEquals(155, response.getCardUsageCount());
+    }
+
+    @Test
+    public void testAnalyzeTransaction_CardUsageUnderThresholdAndAmountRatioOverThreshold_ReturnsDeclinedResponse() {
+        // Arrange
+        Transaction transaction = new Transaction();
+        transaction.setCardNum(1234567890123456L);
+        transaction.setAmount(9000.0);
+
+        List<Integer> cardUsageCounts = Arrays.asList(2, 0, 0, 0, 0, 0, 0);
+
+        // Act
+        TransactionAnalysisResponse response = transactionAnalysisService.analyzeTransaction(transaction, cardUsageCounts);
+
+        // Assert
+        assertEquals("Declined", response.getTransactionStatus());
+        assertEquals("1234********3456", response.getCardNumber());
+        assertEquals(9000.0, response.getTransactionAmount());
+        assertEquals(2, response.getCardUsageCount());
+    }
+
+    @Test
+    public void testAnalyzeTransaction_CardUsageUnderThresholdAndAmountRatioUnderThreshold_ReturnsApprovedResponse() {
+        // Arrange
+        Transaction transaction = new Transaction();
+        transaction.setCardNum(1234567890123456L);
+        transaction.setAmount(2000.0);
+
+        List<Integer> cardUsageCounts = Arrays.asList(2, 5, 8, 3, 4, 1, 7);
+
+        // Act
+        TransactionAnalysisResponse response = transactionAnalysisService.analyzeTransaction(transaction, cardUsageCounts);
+
+        // Assert
+        assertEquals("Approved", response.getTransactionStatus());
+        assertEquals("1234********3456", response.getCardNumber());
+        assertEquals(2000.0, response.getTransactionAmount());
+        assertEquals(30, response.getCardUsageCount());
+    }
+}
